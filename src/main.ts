119aa3d98810d5d12f5c643b224a4090cb374370
Josh Bernitt <jjbernitt@gmail.com>
2016-06-16 19:49:57 -0400

    playing with routers
    trying to get routers to work, but finding out they don’t yet…

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment,  } from './app/';
-import { ThreeDayOutlookComponent } from './app/three-day-outlook/index';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 if (environment.production) {
   enableProdMode();
